a28f1477f8c4a9043a05bf5e78be4c3ab916c499
Tareq1333 <developer1@bytechjo.com>
2025-11-25 11:39:27 +0300

    Scorm package

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import "animate.css/animate.min.css";
+import { ScormProvider } from '@code-by-dwayne/react-scorm-provider';
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <ScormProvider>
+      <App />
+    </ScormProvider>
+  </BrowserRouter>
+  </StrictMode>
 )
